0db47e9f1fc216741459b7eb3168e3b7f298a7d3
Ekaterina Schedlberger <ek.schedl@gmail.com>
2024-04-03 11:47:26 +0200

    10c

--- script_glo.js
+++ script_glo.js
@@ -1,28 +1,15 @@
 "use strict";
 
-console.log(typeof String(true));
-console.log(typeof String(undefined));
-console.log(typeof String(null));
-console.log(typeof String("123"));
-console.log(typeof String(123));
+const lists = document.querySelectorAll('.list')
+const elems = document.querySelectorAll('.elem')
 
-console.log(typeof Number(true));
-console.log(typeof Number(undefined));
-console.log(typeof Number(null));
-console.log(typeof Number("123"));
-console.log(typeof Number(123));
+console.log(lists)
+console.log(elems)
 
-console.log(5 + "5");
-console.log(5 - "5");
+const newElem = document.createElement('li')
 
-console.log(5 * "5");
+newElem.classList.add('elem')
+newElem.textContent = "step 9 create Element"
+lists[1].append(newElem)
 
-console.log(5 / "5");
-
-console.log(5 == "5");
-console.log(5 === "5");
-//let answer = confirm('Are you 18 years old?')
-//console.log(answer);
-
-//let answer2 = prompt('How old are you?', '18 years old') //  2.parametr- default
-// //console.log(answer2); // answer ist a string always
+console.log(newElem)
