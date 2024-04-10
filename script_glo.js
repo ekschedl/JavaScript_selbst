6b3b11c9dc3463f0d154978a2f8f7d416779d82c
Ekaterina Schedlberger <ek.schedl@gmail.com>
2024-04-10 11:38:01 +0200

    start 3

--- script_glo.js
+++ script_glo.js
@@ -1,3 +1,28 @@
 "use strict";
 
+console.log(typeof String(true));
+console.log(typeof String(undefined));
+console.log(typeof String(null));
+console.log(typeof String("123"));
+console.log(typeof String(123));
 
+console.log(typeof Number(true));
+console.log(typeof Number(undefined));
+console.log(typeof Number(null));
+console.log(typeof Number("123"));
+console.log(typeof Number(123));
+
+console.log(5 + "5");
+console.log(5 - "5");
+
+console.log(5 * "5");
+
+console.log(5 / "5");
+
+console.log(5 == "5");
+console.log(5 === "5");
+//let answer = confirm('Are you 18 years old?')
+//console.log(answer);
+
+//let answer2 = prompt('How old are you?', '18 years old') //  2.parametr- default
+// //console.log(answer2); // answer ist a string always
