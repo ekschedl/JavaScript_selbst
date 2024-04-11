4858acc38e73c39efbe9a854c2482bd48f247076
Ekaterina Schedlberger <ek.schedl@gmail.com>
2024-04-11 21:47:33 +0200

    as in video

--- script_glo.js
+++ script_glo.js
@@ -1,28 +1,55 @@
 "use strict";
 
-console.log(typeof String(true));
-console.log(typeof String(undefined));
-console.log(typeof String(null));
-console.log(typeof String("123"));
-console.log(typeof String(123));
+const user ={
+    name: 'Alex',
+    age: 18,
+    isAdult: false,
+    projects: {
+        firstProject: 'firstProject',
+        secondProject: 'secondProject',
+    }
+}
 
-console.log(typeof Number(true));
-console.log(typeof Number(undefined));
-console.log(typeof Number(null));
-console.log(typeof Number("123"));
-console.log(typeof Number(123));
+// const name = user.name
+// const age = user.age
+// const isAdult = user.isAdult
+// выполним тоже самое  но только через деструктуризацию
 
-console.log(5 + "5");
-console.log(5 - "5");
+const {name: nameUser, age, isAdult, projects, projects:{firstProject, secondProject}} = user;
 
-console.log(5 * "5");
+console.log(nameUser);
+console.log(age);
+console.log(isAdult);
+console.log(user.name);
+console.log(secondProject);
+console.log(firstProject)
 
-console.log(5 / "5");
 
-console.log(5 == "5");
-console.log(5 === "5");
-//let answer = confirm('Are you 18 years old?')
-//console.log(answer);
+// пример 2
+const names = ['katja', 'olija', 'andrey', ['tanja', 'luda']]
+const katja = names[0]
+const olija = names[1]
+console.log(katja);
+console.log(olija);
+// упрощаем через деструктуризацию
+const [katja1, olja, andrey, [tanja, luda]] = names
 
-//let answer2 = prompt('How old are you?', '18 years old') //  2.parametr- default
-// //console.log(answer2); // answer ist a string always
+console.log(katja1);
+console.log(olja);
+console.log(andrey);
+console.log(tanja);
+console.log(luda);
+
+// пример 3
+const logger = (first, second, third) => {
+    console.log(`${first} ${second} ${third}`)
+}
+logger ('i', 'love', 'you')
+
+const logger2 = (obj) => {
+    const {first, second, third} = obj
+    console.log(first)
+}
+// logger ('i', 'love', 'you')
+logger2({first:'i', second: 'love', third:'you'})
+
