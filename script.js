9ee9d774cfcd3b3b8bf209989c764d2cc374ce84
Ekaterina Schedlberger <ek.schedl@gmail.com>
2024-04-18 17:58:32 +0200

    as in video

--- script.js
+++ script.js
@@ -1,5 +1,52 @@
 
 
 "use strict"
-const text =document.getElementById('text')
-console.log(text);
+// const text =document.getElementById('text')
+// console.log(text);
+
+// // text.textContent =text.textContent.replace(/Lorem/gi, "лорем")
+
+
+// text.innerHTML = text.innerHTML.replace(/<span>[\w\s]+<\/span>/gi, (str) => {
+//     return `<strong>${str}</strong>`;
+//   });
+  
+
+
+
+// работа с формой
+const textInput =document.getElementById("text")
+const numberInput =document.getElementById("number")
+const formInput =document.getElementById("form")
+
+
+// //
+// textInput.addEventListener('input', (e) => {
+//     e.target.value= e.target.value.replace(/\d+/, "" )
+//     console.log(e.target.value);
+//    // console.log(e.target.value.replace(/\d+/, "" ));// запретили вводить цифры
+// })
+// numberInput.addEventListener('input', (e) => {
+//     e.target.value= e.target.value.replace(/\D+/, "" )
+//     console.log(e.target.value);
+//     //e.target.value= e.target.value.replace(/\D+/, "" )// запретили вводить буквы и англ е
+// })
+// console.log(textInput);
+
+formInput.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let isError = false
+
+    if(!/[^а-яА-Я]/g.test(textInput.value) && textInput.value !== "") {
+        alert("в инпуте только кирилица")
+    } else {isError = true}
+       
+    if(!/[^\d]/g.test(numberInput.value)  && numberInput.value !== "") {
+        alert("в инпуте только числа")
+    }else {isError = true}
+ 
+
+    if (!isError){
+    alert("данные отправлены")
+    }
+})
