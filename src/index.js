d8fca0cb87215b0639026e81e278e63caaa80330
Ekaterina Schedlberger <ek.schedl@gmail.com>
2024-04-23 13:09:14 +0200

    neuer start

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import timer from "./modules/timer";
+import menu from "./modules/menu";
+import modal from "./modules/modal";
+import calc from "./modules/calc";
+import form from "./modules/form";
+import tabs from "./modules/tabs";
+import slider from "./modules/slider";
+
+timer("21 april 2024, 21:36:00");
+menu();
+modal();
+calc();
+form();
+tabs();
+slider();
