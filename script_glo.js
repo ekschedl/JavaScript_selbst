ffb024e345cc55688cc9ab67f600b9b712ef7c27
Ekaterina Schedlberger <ek.schedl@gmail.com>
2024-04-26 12:00:35 +0200

    done

--- script_glo.js
+++ script_glo.js
@@ -1,28 +1,77 @@
 "use strict";
 
-console.log(typeof String(true));
-console.log(typeof String(undefined));
-console.log(typeof String(null));
-console.log(typeof String("123"));
-console.log(typeof String(123));
+//асинхронный код
+console.log(1);
+setTimeout(() =>{
+    console.log(2);
+}, 2000)
 
-console.log(typeof Number(true));
-console.log(typeof Number(undefined));
-console.log(typeof Number(null));
-console.log(typeof Number("123"));
-console.log(typeof Number(123));
+setTimeout(() =>{
+    console.log(4);
+}, 1000)
 
-console.log(5 + "5");
-console.log(5 - "5");
 
-console.log(5 * "5");
 
-console.log(5 / "5");
 
-console.log(5 == "5");
-console.log(5 === "5");
-//let answer = confirm('Are you 18 years old?')
-//console.log(answer);
+//promise / resulte в случае успехаб reject  в случае неудачи отрабатывае
+const promise = (num)=> {
+    return new Promise((resolve, reject)=> {
+        setTimeout(()=>{
+        
+            if(num>10){
+                resolve(num)
+             } else {
+                reject("Some error")
+             }
+        }, 2000)
+        })
+}
 
-//let answer2 = prompt('How old are you?', '18 years old') //  2.parametr- default
-// //console.log(answer2); // answer ist a string always
+promise(15)
+//    // then срабатывает  при     resolve
+//    .then((data) => data +10)
+//    .then((newData) => {
+//         console.log(newData);
+// })
+//    // catch vtnод для перехватат ошибок
+//    .catch((errorMess) => console.log(errorMess))
+// //    .finally(()=> console.log("finaly"))
+
+//___
+// .then((data) => {
+//     console.log(data);
+//     return new Promise((resolve, reject) => {
+//         setTimeout(()=>{
+//         resolve(data+10)
+//         }, 2000)
+//     })
+// })
+// .then((data)=> {
+// console.log(data);
+// })
+// .catch((errorMess) => console.log(errorMess))
+//________
+
+
+//Example2
+const promise2 = (num)=> {
+    return new Promise((resolve, reject)=> {
+        setTimeout(()=>{
+        
+            if(num>10){
+                resolve(num)
+             } else {
+                reject(num)
+             }
+        }, 2000)
+        })
+}
+
+const one=promise2(5)
+const two=promise2(25)
+const three=promise2(35)
+Promise.all([one, two, three])
+.then((data)=>{
+    console.log(data);
+})
+.catch(error => console.log(error))
