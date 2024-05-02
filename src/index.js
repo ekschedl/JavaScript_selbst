e010cc4078257b62a6704f5e4750c0d43811e1f4
Ekaterina Schedlberger <ek.schedl@gmail.com>
2024-05-02 12:11:33 +0200

    delete done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-
 // index.js
 import { render } from "./modules/render";
-import { UserService } from "./modules/usersService";
+import { UserService } from "./modules/userService";
 import { addUsers } from "./modules/addUsers";
+import { removeUsers } from "./modules/removeUsers";
 
-window.userService = new UserService
+window.userService = new UserService();
 userService.getUsers().then(data => {
-
     render(data);
-
-})
+});
 addUsers();
+removeUsers();
--- src/modules/addUsers.js
+++ src/modules/addUsers.js
@@ -1,24 +1,27 @@
-import { UserService } from "./usersService"
+// addUsers.js
+import { render } from "./render";
+import { UserService } from "./userService"; 
 
-export const addUsers =() =>{
-    const form =document.querySelector("form")
-    const nameInput =form.querySelector('#form-name')
-    const emailInput =form.querySelector('#form-email')
-    const childrenInput =form.querySelector('#form-children')
+export const addUsers = () => {
+    const form = document.querySelector("form");
+    const nameInput = form.querySelector('#form-name');
+    const emailInput = form.querySelector('#form-email');
+    const childrenInput = form.querySelector('#form-children');
 
-    console.log(nameInput);
-    form.addEventListener('submit', (e)=> {
-        e.preventDefault()
-       
- 
-
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        
         const user = {
-       
             name: nameInput.value,
             email: emailInput.value,
             children: childrenInput.checked,
             permission: false
-        }
-        userService.addUser(user)
-    })
+        };
+        
+        userService.addUser(user).then(() => { 
+            userService.getUsers().then(users => {
+                render(users);
+            });
+        });
+    });
 }
